77531c974b1b796bd1e087e6e6dfa4ef5cc5207d
unknown <gueye.emma@gmail.com>
2020-10-16 12:23:48 +0200

    clean app.js

--- app.js
+++ app.js
@@ -22,16 +22,13 @@ const {
 app.use(cors())
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(
   session({
@@ -48,6 +45,7 @@ app.use(
   })
 );
 
+app.get("/", (req, res) => res.send("afg backend is running !"));
 //console.log("foo");
 //console.log("foo");
 
